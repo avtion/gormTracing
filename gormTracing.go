e31faf36ec3b745abe71db01186ae207c76fb4fd
Avtion <634134753@qq.com>
2020-08-15 02:04:37 +0800

    🔧 更新

--- gormTracing.go
+++ gormTracing.go
@@ -1,7 +1,6 @@
 package gormTracing
 
 import (
-	"fmt"
 	"github.com/opentracing/opentracing-go"
 	tracerLog "github.com/opentracing/opentracing-go/log"
 	"gorm.io/gorm"
@@ -16,23 +15,23 @@ const (
 func before(db *gorm.DB) {
 	// 先从父级spans生成子span
 	span, _ := opentracing.StartSpanFromContext(db.Statement.Context, "gorm")
-	// 利用db实例去
+	// 利用db实例去传递span
 	db.InstanceSet(gormSpanKey, span)
 	return
 }
 
 func after(db *gorm.DB) {
+	// 从GORM的DB实例中取出span
 	_span, isExist := db.InstanceGet(gormSpanKey)
 	if !isExist {
 		return
 	}
+
+	// 断言进行类型转换
 	span, ok := _span.(opentracing.Span)
 	if !ok {
 		return
 	}
-	defer func() {
-		db.Statement.Settings.Delete(fmt.Sprintf("%p", db.Statement) + gormSpanKey)
-	}()
 	defer span.Finish()
 
 	// Error
@@ -45,7 +44,14 @@ func after(db *gorm.DB) {
 	return
 }
 
-func register(db *gorm.DB) {
+type OpentracingPlugin struct{}
+
+func (op *OpentracingPlugin) Name() string {
+	return "opentracingPlugin"
+}
+
+func (op *OpentracingPlugin) Initialize(db *gorm.DB) (err error) {
+	// 开始前
 	db.Callback().Create().Before("gorm:before_create").Register(callBackBeforeName, before)
 	db.Callback().Query().Before("gorm:query").Register(callBackBeforeName, before)
 	db.Callback().Delete().Before("gorm:before_delete").Register(callBackBeforeName, before)
@@ -53,10 +59,14 @@ func register(db *gorm.DB) {
 	db.Callback().Row().Before("gorm:row").Register(callBackBeforeName, before)
 	db.Callback().Raw().Before("gorm:raw").Register(callBackBeforeName, before)
 
-	db.Callback().Create().After("gorm:after_create").Register(callBackBeforeName, after)
-	db.Callback().Query().After("gorm:after_query").Register(callBackBeforeName, after)
-	db.Callback().Delete().After("gorm:after_delete").Register(callBackBeforeName, after)
-	db.Callback().Update().After("gorm:after_update").Register(callBackBeforeName, after)
-	db.Callback().Row().After("gorm:row").Register(callBackBeforeName, after)
-	db.Callback().Raw().After("gorm:raw").Register(callBackBeforeName, after)
+	// 结束后
+	db.Callback().Create().After("gorm:after_create").Register(callBackAfterName, after)
+	db.Callback().Query().After("gorm:after_query").Register(callBackAfterName, after)
+	db.Callback().Delete().After("gorm:after_delete").Register(callBackAfterName, after)
+	db.Callback().Update().After("gorm:after_update").Register(callBackAfterName, after)
+	db.Callback().Row().After("gorm:row").Register(callBackAfterName, after)
+	db.Callback().Raw().After("gorm:raw").Register(callBackAfterName, after)
+	return
 }
+
+var _ gorm.Plugin = &OpentracingPlugin{}
--- gormTracing_test.go
+++ gormTracing_test.go
@@ -1,19 +1,27 @@
 package gormTracing
 
 import (
+	"context"
+	"fmt"
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"io"
+	"math/rand"
+	"strconv"
+	"sync"
 	"testing"
+	"time"
 )
+
+const dsn = "root:root@tcp(localhost:10012)/test?charset=utf8mb4&parseTime=True&loc=Local"
 
 func initJaeger() (closer io.Closer, err error) {
 	// 根据配置初始化Tracer 返回Closer
 	tracer, closer, err := (&config.Configuration{
-		ServiceName: "xormWithTracing",
+		ServiceName: "gormTracing",
 		Disabled:    false,
 		Sampler: &config.SamplerConfig{
 			Type: jaeger.SamplerTypeConst,
@@ -34,11 +42,92 @@ func initJaeger() (closer io.Closer, err error) {
 	return
 }
 
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func Test_GormTracing(t *testing.T) {
-	dsn := "root:root@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	closer, err := initJaeger()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer closer.Close()
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		t.Fatal(err)
 	}
-	register(db)
+	_ = db.Use(&OpentracingPlugin{})
+
+	// 迁移 schema
+	_ = db.AutoMigrate(&Product{})
+
+	// 生成新的Span - 注意将span结束掉，不然无法发送对应的结果
+	span := opentracing.StartSpan("gormTracing unit test")
+	defer span.Finish()
+
+	// 把生成的Root Span写入到Context上下文，获取一个子Context
+	ctx := opentracing.ContextWithSpan(context.Background(), span)
+	session := db.WithContext(ctx)
+
+	// Create
+	session.Create(&Product{Code: "D42", Price: 100})
+
+	// Read
+	var product Product
+	session.First(&product, 1)                 // 根据整形主键查找
+	session.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+
+	// Update - 将 product 的 price 更新为 200
+	session.Model(&product).Update("Price", 200)
+	// Update - 更新多个字段
+	session.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
+	session.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// Delete - 删除 product
+	session.Delete(&product, 1)
+}
+
+func Test_GormTracing2(t *testing.T) {
+	closer, err := initJaeger()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer closer.Close()
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = db.Use(&OpentracingPlugin{})
+
+	rand.Seed(time.Now().UnixNano())
+
+	num, wg := 1<<10, &sync.WaitGroup{}
+
+	wg.Add(num)
+
+	for i := 0; i < num; i++ {
+		go func(t int) {
+			span := opentracing.StartSpan(fmt.Sprintf("gormTracing unit test %d", t))
+			defer span.Finish()
+
+			ctx := opentracing.ContextWithSpan(context.Background(), span)
+			session := db.WithContext(ctx)
+
+			p := &Product{Code: strconv.Itoa(t), Price: uint(rand.Intn(1 << 10))}
+
+			session.Create(p)
+
+			session.First(p, p.ID)
+
+			session.Delete(p, p.ID)
+
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
 }
